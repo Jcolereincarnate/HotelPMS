eba52e405611aedbf6dfde65d46e37543ff4d05d
Okubule Temitope <topepaul38@gmail.com>
2026-09-12 17:54:12 +0100

    Fixed URL mismatch

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('reservation', views.reservation, name='reservation'),
-    path('health/', views.health, name='health'),
+    path('health', views.health, name='health'),
 ]
